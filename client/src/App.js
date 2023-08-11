96c1c5316bd09ab01d19c50a4cae397ff83fa6ff
Batuhan Ayaz <batuhan.ayaz@student.ehb.be>
2023-08-11 10:15:36 +0200

    Pages created + creating routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import Header from './components/Header';
+import "./App.css";
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
+      <main className="pt-20 bg-slate-100 min-h-[calc(100vh)]">
+        <Outlet />
+      </main>
     </div>
-
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import Logo from '../assets/logo.png'
-import { Link } from 'react-router-dom'
-import { FaUserAlt } from 'react-icons/fa'
-import { FaCartPlus } from 'react-icons/fa'
+import React from "react";
+import { useState } from "react";
+import Logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import { FaUserAlt } from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleShowMenu = () => {
+    setShowMenu((preve) => !preve);
+  };
+
   return (
     <header className="fixed shadow-md w-full h-50 px-2 md:px-4 z-50 bg-white">
       {/* Desktop and Mobile Friendly*/}
@@ -15,24 +22,37 @@ const Header = () => {
         </div>
         {/* NAVBAR */}
         <div className="flex items-center gap-4 md:gap-7">
-          <nav className='flex gap-4 md:gap-6 text-base md:text-lg text-secondary font-semibold'>
+          <nav className="flex gap-4 md:gap-6 text-base md:text-lg text-secondary font-semibold">
             <Link to={""}>Home</Link>
             <Link to={"menu"}>Menu</Link>
             <Link to={"about"}>About</Link>
             <Link to={"contact"}>Contact</Link>
           </nav>
 
-          <div className="text-2xl text-primary">
-            <FaCartPlus/>
+          <div className="text-2xl text-primary relative cursor-pointer">
+            <FaCartPlus />
+            <div className="absolute -top-2 -right-2 text-white bg-red-500 h-4 w-4 rounded-full m-0 p-0 text-sm text-center">
+              0
+            </div>
           </div>
 
-          <div className="text-2xl text-primary">
-            <FaUserAlt color='#0096D6' />
+          <div className="text-primary cursor-pointer" onClick={handleShowMenu}>
+            <div className="text-2xl">
+              <FaUserAlt color="#0096D6" />
+            </div>
+            {showMenu && (
+              <div className="absolute right-2 bg-white py-2 px-2 shadow-md flex flex-col">
+                <Link to={"newProduct"} className="whitespace-nowrap">
+                  New product
+                </Link>
+                <Link to={"login"} className="whitespace-nowrap">
+                  Login
+                </Link>
+              </div>
+            )}
           </div>
         </div>
       </div>
-
-      {/* {Mobile} */}
     </header>
   );
 };
