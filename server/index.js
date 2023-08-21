5ad3012a7bb2dc2bc9d53d0899adf77e6c7ae525
Batuhan Ayaz <batuhan.ayaz@student.ehb.be>
2023-08-21 16:16:18 +0200

    Docker installed. Tested Working.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
